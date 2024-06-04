32143b7cd7db7ebf81ca2a72195897838b1ab3c4
Tj545 <thejastanvi2.0@gmail.com>
2024-06-04 11:03:55 +0530

    create pages and routes

--- cilent/src/App.jsx
+++ cilent/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Projects from './pages/Projects'
+import Dashboard from './pages/Dashboard'
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='about' element={<About/>}/>
+      <Route path='sign-in' element={<Signin/>}/>
+      <Route path='sign-out' element={<Signup/>}/>
+      <Route path='projects' element={<Projects/>}/>
+      <Route path='dashboard' element={<Dashboard/>}/>
+   
+    </Routes>
+  </BrowserRouter>
   )
 }
